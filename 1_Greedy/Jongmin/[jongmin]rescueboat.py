d68f38332d3144179c00fa1b65e0199bdf53659f
alertjjm <sdr2936@gmail.com>
2021-03-16 23:32:51 +0900

    [FAIL] efficiency

--- 1_Greedy/Jongmin/[jongmin]rescueboat.py
+++ 1_Greedy/Jongmin/[jongmin]rescueboat.py
@@ -1,17 +1,23 @@
 def solution(people, limit):
-    answer=0
     people.sort(reverse=True)
-    i=0
-    while(i<len(people)):
-        if(people[i]<=limit):
-            value=people[i]
-            del people[i]
-            i-=1
-            t=solution(people,limit-value)
-            answer+=1
-        i+=1
+    visited=[0 for i in range(len(people))]
+    answer=greedy(people,visited,limit)
     return answer
-
+def greedy(people, visited, limit):
+    answer=0
+    for i in range(len(people)):
+        if(0 not in visited):
+            return answer
+        if(visited[i]==0 and people[i]<=limit):
+            visited[i]=1
+            for j in range(len(people)-1,i,-1):
+                if (visited[j] == 0 and people[j] <= limit-people[i]):
+                    visited[j] = 1
+                    break
+            answer+=1
+    return answer
 print(solution([70, 50, 80, 50],100))
 print(solution([70, 80, 50],100))
 print(solution([10,20,30,40,50,60,70,80,90], 100))
+print(solution([20, 50, 50, 80], 100))
+print(solution([40, 40, 40],100))
