78e327c0adcc97e7a78cc59c9aa580bbad5ddb02
maybethiswilldo <cks0229@hanmail.net>
2021-04-28 18:05:28 +0900

    move

--- 2_Simulation/maybe/makeGame.py
+++ 2_Simulation/maybe/makeGame.py
@@ -1,1 +1,9 @@
-# writing..
+class Hero:
+    def __init__(self, coordinates, direction):
+        self.coordinates = coordinates
+        self.direction = direction
+
+    def peek(self):
+        self.direction -= 1
+        if self.direction == 0:
+            
