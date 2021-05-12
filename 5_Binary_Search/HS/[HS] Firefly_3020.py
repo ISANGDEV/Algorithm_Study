cb607bba8aa1b827d5e87ed3adfc8d8531a0612e
KangHyeonSu1202 <80747415+KangHyeonSu1202@users.noreply.github.com>
2021-05-12 22:32:41 +0900

    Update [HS] Firefly_3020.py

--- 5_Binary_Search/HS/[HS] Firefly_3020.py
+++ 5_Binary_Search/HS/[HS] Firefly_3020.py
@@ -4,12 +4,13 @@ def binary(obstacle,target):
     right = len(obstacle) - 1
     while left <= right:
         middle = (left + right) // 2
-        if obstacle[middle] > target:
+        if obstacle[middle] > target: # 장애물이 부딪히는가
             right = middle - 1
-        elif obstacle[middle] < target:
+        elif obstacle[middle] < target: # 확인하고
             left = middle + 1
 
-    return len(obstacle) - right - 1
+    return len(obstacle) - right - 1 # 부딪히는 장애물 다음부터는 다 부딪히니까
+# 전체 갯수에서 - 부딪힌 장애물 index 빼주고, 0에서 index가 시작하니까 1 더 빼주고
 
     # 123 456
     # len : 6 / middle = 2 2.5
@@ -23,7 +24,8 @@ for i in range(n):
     if i % 2 == 0 :
         obstacle_bottom.append(int(sys.stdin.readline()))
     else:
-        obstacle_top.append(int(sys.stdin.readline()))
+        obstacle_top.append(int(sys.stdin.readline())) # 맨처음에 h - 입력 받은 수로 했다가, 틀린거 깨달았음
+        # 어차피 밑에서 입력 해줄 때 h - i 로 해줄꺼라서 그냥 "높이 값" 자체를 넣어야함
 
 obstacle_top.sort()
 obstacle_bottom.sort()
@@ -34,8 +36,10 @@ answer = n
 count = 0
 for i in range(0,h):
     low = binary(obstacle_bottom, i + 0.5)
-    high = binary(obstacle_top, h - i - 0.5)
-
+    high = binary(obstacle_top, h - i - 0.5) # 중간을 지니가니까 +- 0.5 해주고
+    # 높이 별로 최솟값 확인
+    # N개의 높이에서 N개의 장애물 일일히 확인하기 -> 정렬된 배열을 이진탐색으로 찾아서 이거에 부딪히는 장애물 찾기(log N) 맞나? 어쨋든 많이 줄여서
+    # N^2을 피했음
     if answer > low + high:
         answer = low + high
         count = 1
