b94e2abe77aa8453f93d54df75b81d0cf6259d31
HyunMinSam <hyunmin4287@naver.com>
2021-07-06 13:17:57 +0900

    CH11_5

---
+++ 11_Test_Greedy/Hyunmin/CH11_5.py
@@ -0,0 +1,14 @@
+import sys
+
+n, m = sys.stdin.readline().rstrip().split()
+data = list(map(int, sys.stdin.readline().rstrip().split()))
+
+cnt = 0
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+        if data[i] == data[j]:
+            continue
+        cnt += 1
+
+print(cnt)
+
