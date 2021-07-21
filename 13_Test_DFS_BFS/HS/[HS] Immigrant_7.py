afe2aa89fe5554238a6d6f90d6da993ea35c7e71
KangHyeonSu1202 <80747415+KangHyeonSu1202@users.noreply.github.com>
2021-07-21 22:57:13 +0900

    Update [HS] Immigrant_7.py

--- 13_Test_DFS_BFS/HS/[HS] Immigrant_7.py
+++ 13_Test_DFS_BFS/HS/[HS] Immigrant_7.py
@@ -3,34 +3,37 @@ from collections import deque
 
 def bfs(x,y,day,geo_1,geo_2,n):
     global visit
-
+    
+    save_pos = []
+    visit_temp = [[0 for i in range(n)] for j in range(n)]
     if day % 2 == 0:
         que = deque()
         count = 1
         que.append([x,y])
         temp = geo_1[x][y]
         visit[x][y] = 1
-        geo_1[x][y] = 101
+        visit_temp[x][y] = 1
 
         while que:
+
             a,b = que.popleft()
             for i,j in vec:
                 new_x = a + i
                 new_y = b + j
                 if 0<= new_x < n and 0 <= new_y < n :
-                    if l<= abs(geo_1[a][b] - geo_1[new_x][new_y]) <=r and geo_1[new_x][new_y] != 101:
+                    if l<= abs(geo_1[a][b] - geo_1[new_x][new_y]) <=r and visit_temp[new_x][new_y] == 0:
                         que.append([new_x,new_y])
                         count += 1
                         temp += geo_1[new_x][new_y]
                         visit[new_x][new_y] = 1
-                        geo_1[new_x][new_y] = 101
+                        visit_temp[new_x][new_y] = 1
         if count == 1:
             return count
 
 
         for i in range(n):
             for j in range(n):
-                if geo_1[i][j] == 101:
+                if visit_temp[i][j] == 1:
                     geo_2[i][j] = temp // count
 
         return count
@@ -41,7 +44,7 @@ def bfs(x,y,day,geo_1,geo_2,n):
         que.append([x,y])
         temp = geo_2[x][y]
         visit[x][y] = 1
-        geo_2[x][y] = 101
+        visit_temp[x][y] = 1
 
         while que:
             a,b = que.popleft()
@@ -49,19 +52,18 @@ def bfs(x,y,day,geo_1,geo_2,n):
                 new_x = a + i
                 new_y = b + j
                 if 0<= new_x < n and 0 <= new_y < n :
-                    if (l <= abs(geo_2[a][b] - geo_2[new_x][new_y]) <= r) and geo_2[new_x][new_y] != 101:
+                    if (l <= abs(geo_2[a][b] - geo_2[new_x][new_y]) <= r) and (visit_temp[new_x][new_y] == 0):
                         que.append([new_x,new_y])
                         count += 1
                         temp += geo_2[new_x][new_y]
                         visit[new_x][new_y] = 1
-                        geo_2[new_x][new_y] = 101
-
+                        visit_temp[new_x][new_y] = 1
         if count == 1:
             return count
-
+        
         for i in range(n):
             for j in range(n):
-                if geo_2[i][j] == 101:
+                if visit_temp[i][j] == 1:
                     geo_1[i][j] = temp // count
 
         return count
@@ -92,14 +94,12 @@ while 1:
 
     elif day % 2 == 1:
         geo_1 = [item[:] for item in geo_2]
-
         for i in range(n):
             for j in range(n):
                 if visit[i][j] == 1:
                     temp += 1
                 else:
                     temp += bfs(i, j, day, geo_1,geo_2,n)
-
     if temp == n*n:
         break
     elif temp != n*n:
