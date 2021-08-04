2a828571618605946fa79800d8741e701db91db1
HyunMinSam <hyunmin4287@naver.com>
2021-08-04 20:18:57 +0900

    CH15_4 미완성

--- 15_Test_Binary_Sort/Hyunmin/CH15_4.py
+++ 15_Test_Binary_Sort/Hyunmin/CH15_4.py
@@ -9,7 +9,7 @@ def solution(words, queries):
     # word의 길이에 해당하는 idx에 단어 삽입
     for word in words: 
         words_lists[len(word)].append(word)
-    temp = 1 
+     
     for words in words_lists: 
         if words: 
             words.sort() 
@@ -18,20 +18,21 @@ def solution(words, queries):
         # fro??  
         if query[0] != '?': 
             start_idx = 0 
-            end_idx = bisect.bisect_left(query, "?")  # 3
-            print("st= ", start_idx, "end=", end_idx)
+            end_idx = 0 
+            for end_idx in range(1, words_idx): 
+                if query[end_idx] == '?': 
+                    break
+
             # 찾는 값이 있으면 -1 보다 큰 값 리턴
-            # 이렇게 하면 안된다 -> 문자열이 정렬되어야 한다 -> 다른방법 find.
-            print(query, query[start_idx:end_idx])
+            # 찾는 값이 있으면 -1 보다 큰 값 리턴
             print(words_lists[words_idx], query[start_idx:end_idx], words_idx, start_idx, end_idx)
             left = binary_search_left(words_lists[words_idx], query[start_idx:end_idx], words_idx, start_idx, end_idx)
             right = binary_search_right(words_lists[words_idx], query[start_idx:end_idx], words_idx, start_idx, end_idx)
-
+            print(left , right)
             if right >= 0 and left >= 0:
                 print(right-left)
                 answer.append(right-left)
-            
-
+           # 68 79 30  
                 
         else: 
             pass 
@@ -43,17 +44,14 @@ def binary_search_left(w_l, q, w_i, start_slice, end_slice):
     w_l_len = len(w_l)
     while start <= end: 
         mid = (start + end) // 2
-        # python 은 mid 0일때 w_l -1 검사해도 괜찮은 걸로 안다. -> -1이 맨뒤에있는 값을 찾기 때문이였다. 
-        if w_l[mid][start_slice:end_slice] >= q: 
-            if mid == 0: 
-                return mid
-            elif mid > 0 and mid < w_l_len:
-                if w_l[mid-1][start_slice:end_slice] != q: 
-                    return mid 
-                    
+        if (mid == 0 or q > w_l[mid - 1]) and w_l[mid] == q: 
+        # if w_l[mid][start_slice:end_slice] == q and (mid == 0 or q > w_l[mid - 1]): 
+            return mid 
+        elif w_l[mid] >= q: 
             end = mid - 1
         else: 
             start = mid + 1
+
     return -1
 
 def binary_search_right(w_l, q, w_i, start_slice, end_slice):
@@ -63,16 +61,13 @@ def binary_search_right(w_l, q, w_i, start_slice, end_slice):
     while start <= end: 
         mid = (start + end) // 2
         # python 은 mid 0일때 w_l -1 검사해도 괜찮은 걸로 안다. -> -1이 맨뒤에있는 값을 찾기 때문이였다. 
-        if w_l[mid][start_slice:end_slice] <= q: 
-            if mid == w_l - 1: 
-                return mid
-            elif mid >= 0 and mid < w_l - 2:
-                if w_l[mid+1][start_slice:end_slice] != q: 
-                    return mid 
-                    
-            start = mid + 1
+        # if w_l[mid][start_slice:end_slice] == q and (mid == w_l_len-1 or q < w_l[mid+1]): 
+        if (mid == w_l_len-1 or q < w_l[mid+1]) and w_l[mid][start_slice:end_slice]: 
+            return mid 
+        elif w_l[mid] > q: 
+            end = mid - 1
         else: 
-            end = mid -1 
+            start = mid + 1
     return -1
 
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
