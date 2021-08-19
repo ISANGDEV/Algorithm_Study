bc3ac13558bf0fef0c57f4767d372151452b64f2
HyunMinSam <hyunmin4287@naver.com>
2021-08-19 23:12:13 +0900

    CH17_6

--- 17_Test_Shortest_Path/Hyunmin/CH17_6.py
+++ 17_Test_Shortest_Path/Hyunmin/CH17_6.py
@@ -8,7 +8,6 @@ for j in range(n+1):
     dp[j][0] = j 
 for i in range(m+1): 
     dp[0][i] = i
-print(dp)
 
 # 추가는 왼쪽, 삭제는 위쪽, 교체는 왼쪽위 대각선인데, i -> jl v-> w는 글자 비교할때 같은 것으로 간주 
 for i in range(1, n+1): 
@@ -16,6 +15,7 @@ for i in range(1, n+1):
         # 같을 때, 왼쪽위 대각선 
         if result[i-1] == answer[j-1]: 
             dp[i][j] = dp[i-1][j-1]
+        # 비교를 효율적으로 해야함 
         elif result[i-1] == "i" or answer[j-1] == "v":
             if result[i-1] == "i" and (answer[j-1] == "j" or answer[j-1] == "l" or answer[j-1] == "l"): 
                 # 같은 것으로 간주 
@@ -28,10 +28,5 @@ for i in range(1, n+1):
             dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
                 
 
-score = int(1e9) 
-for i in range(1, n+1):
-    if dp[i][m] < score: 
-        score = dp[i][m]
 
-print("score ", score)
-print(dp)
+print(dp[n-1][m-1])
