83c7a627a859ebadd4bbb7d2167ea482faa6c322
HyunMinSam <hyunmin4287@naver.com>
2021-09-01 14:32:04 +0900

    CH18_1

---
+++ 18_Test_Graph_Theory/Hyunmin/CH18_1.py
@@ -0,0 +1,36 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+N, M = list(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(N)] 
+path = list(map(int, input().split()))
+parent = [0] * (N+1)
+
+for i in range(1, N + 1):
+    parent[i] = i
+
+for i in range(N):
+    for j in range(N): 
+        if arr[i][j]: 
+            union_parent(parent, i + 1, j + 1)
+
+tf = 'YES' 
+for i in range(1, N):
+    if find_parent(parent, i) != find_parent(parent, i + 1): 
+        tf = 'NO'
+        break 
+print(tf)
+
+
+
+
